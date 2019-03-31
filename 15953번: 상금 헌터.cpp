eff4ec0d3c1206501992aec56b7eaf842da5e2e8
Yoon Jihwan <youwlghks@gmail.com>
2019-03-31 19:48:20 +0900

    Create 15953번: 상금 헌터.cpp

---
+++ 15953번: 상금 헌터.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int first_contest[2][6] = {1, 3, 6, 10, 15, 21, 500, 300, 200, 50, 30, 10 };
+	int second_contest[2][5] = {1, 3, 7, 15, 31, 512, 256, 128, 64, 32 };
+	int test_case;
+	cin >> test_case;
+	for (int i = 0; i < test_case; i++)
+	{
+		int first, second, sum = 0;
+		cin >> first;
+		cin >> second;
+		if (first != 0) {
+			for (int j = 0; j < 6; j++) {
+				if ((first - first_contest[0][j]) <= 0) {
+					sum += first_contest[1][j];
+					break;
+				}
+			}
+		}
+		if (second != 0) {
+			for (int j = 0; j < 5; j++) {
+				if ((second - second_contest[0][j]) <= 0) {
+					sum += second_contest[1][j];
+					break;
+				}
+			}
+		}
+		cout << sum * 10000 << endl;
+	}
+	return 0;
+}
