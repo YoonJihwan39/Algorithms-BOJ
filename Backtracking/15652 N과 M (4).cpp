b3c384babe513c639c3b8f16e1dbca27a446d71e
youwlghks <youwlghks@gmail.com>
2020-10-15 17:25:42 +0900

    Solved

---
+++ Backtracking/15652 N과 M (4).cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+int N, M;
+vector<int> v;
+
+void NandM(){
+    if(v.size() == M){
+        for(int i = 0; i < v.size(); i++){
+            cout << v[i] << ' ';
+        }
+        cout << '\n';
+        return;
+    }
+
+
+    for(int i =  v.empty() ? 1 : v.back(); i <= N; i++){
+        v.push_back(i);
+        NandM();
+        v.pop_back();
+    }
+    return;
+}
+
+int main(){
+    cin >> N >> M;
+
+    NandM();
+    return 0;
+}
---
+++ Backtracking/15655 N과 M (6).cpp
@@ -0,0 +1,43 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+
+int N, M;
+vector<int> v;
+vector<int> nums;
+bool visited[9];
+void NandM(){
+    if(v.size() == M){
+        for(int i = 0; i < v.size(); i++){
+            cout << v[i] << ' ';
+        }
+        cout << '\n';
+        return;
+    }
+
+
+    for(int i = 0; i < N; i++){
+        if(visited[i]) continue;
+        if(!v.empty() && nums[i] <= v.back()) continue;
+        visited[i] = 1;
+        v.push_back(nums[i]);
+        NandM();
+        v.pop_back();
+        visited[i] = 0;
+    }
+    return;
+}
+
+int main(){
+    cin >> N >> M;
+    int input;
+    for(int i = 0; i < N; i++){
+        cin >> input;
+        nums.push_back(input);
+    }
+    sort(nums.begin(), nums.end());
+
+    NandM();
+    return 0;
+}
